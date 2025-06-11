9139a135d277bbb25c77795ed8adad9674c812d6
Jean-Philippe <jp.ouvrard@lesoctetslibres.com>
2025-06-11 10:20:46 +0200

    feat: add theme management (toggle light/dark)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { Outlet } from "react-router";
 
+import { ThemeProvider } from "@/lib/context/theme";
 import { ToastContextProvider } from "@/lib/context/toast";
 import { UserProvider } from "@/lib/context/user";
 
 export default function App() {
     return (
-        <ToastContextProvider>
-            <UserProvider>
-                <Outlet />
-            </UserProvider>
-        </ToastContextProvider>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <ToastContextProvider>
+                <UserProvider>
+                    <Outlet />
+                </UserProvider>
+            </ToastContextProvider>
+        </ThemeProvider>
     );
 }
